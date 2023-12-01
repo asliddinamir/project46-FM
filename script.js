96bab553380568833a9c83ffd9b6240bd1452b22
Asliddin Amirov <asliddinamir2@gmail.com>
2023-12-01 10:04:47 +0800

    applied .minus button

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const questionDivs = document.querySelectorAll('.questionDiv')
+const questionDivs = document.querySelectorAll('.questionDiv');
 
 questionDivs.forEach(question => {
     question.addEventListener('click', () => {
         const answer = question.nextElementSibling;
+        const plusIcon = question.querySelector('.plus');
+        const minusIcon = question.querySelector('.minus');
         answer.classList.toggle('hidden');
+
+        if (answer.classList.contains('hidden')) {
+            plusIcon.classList.remove('hidden');
+            minusIcon.classList.add('hidden');
+        } else {
+            plusIcon.classList.add('hidden');
+            minusIcon.classList.remove('hidden');
+        }
     })
-})
+});
